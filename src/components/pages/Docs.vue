f625d03d1fb655d6811c7a13abe4f5da8c6e8344
Barsch2006 <cchr98756@gmail.com>
2023-06-08 21:47:52 +0200

    update

---
+++ src/components/pages/Docs.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import ESBWFooter from "@/components/ESBWFooter.vue";
+
+export default {
+    components: {
+        ESBWFooter
+    }
+};
+</script>
+
+<template>
+    <v-app>
+        <ESBWFooter />
+    </v-app>
+</template>
+
+<style scoped>
+</style>
